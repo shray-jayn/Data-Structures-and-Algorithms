441674e3bca20683a57440b40e0674fb4b695be2
shray-jayn <shrayjayn@gmail.com>
2022-09-09 16:34:38 +0530

    Trie

--- Trie/1707. Maximum XOR With an Element From Array.cpp
+++ Trie/1707. Maximum XOR With an Element From Array.cpp
@@ -2,6 +2,128 @@
 using namespace std;
 typedef long long int ll;
 
-vector<int> maximizeXor(vector<int> &nums, vector<vector<int>> &queries)
+struct Node
 {
+    Node *links[2];
+
+    bool containsKey(int bit)
+    {
+        return (links[bit] != NULL);
+    }
+
+    Node *get(int bit)
+    {
+        return links[bit];
+    }
+
+    void put(int bit, Node *node)
+    {
+        links[bit] = node;
+    }
+};
+
+class Trie
+{
+private:
+    Node *root;
+
+public:
+    Trie()
+    {
+        root = new Node();
+    }
+
+public:
+    void insert(int num)
+    {
+        Node *node = root;
+
+        for (int i = 31; i >= 0; i--)
+        {
+            int bit = ((num >> i) & 1); // checks if ith bit is set or not
+
+            if (!node->containsKey(bit))
+            {
+                node->put(bit, node);
+            }
+
+            node = node->get(bit);
+        }
+    }
+
+public:
+    int getMax(int num)
+    {
+        Node *node = root;
+
+        int maxNum = 0;
+
+        for (int i = 31; i >= 0; i--)
+        {
+            int bit = ((num >> i) & 1); // checks if ith bit is set or not
+
+            if (node->containsKey(1 - bit)) // if it contains opp. bit
+            {
+                maxNum = maxNum | (1 << i); // get the max
+
+                node = node->get(1 - bit);
+            }
+
+            else
+            {
+                node = node->get(bit); // just go to next node
+            }
+        }
+
+        return maxNum;
+    }
+};
+
+vector<int> maximizeXor(vector<int> &arr, vector<vector<int>> &queries)
+{
+
+    int n = arr.size();
+
+    sort(arr.begin(), arr.end());
+
+    vector<pair<int, pair<int, int>>> offlineQueries;
+
+    int q = queries.size();
+
+    for (int i = 0; i < q; i++)
+    {
+        offlineQueries.push_back({queries[i][1], {queries[i][0], i}}); // <ai,xi, i>
+    }
+
+    sort(offlineQueries.begin(), offlineQueries.end());
+
+    vector<int> ans(q, 0);
+
+    int idx = 0;
+
+    Trie trie;
+
+    for (int i = 0; i < q; i++)
+    {
+        int ai = offlineQueries[i].first;
+        int xi = offlineQueries[i].second.first;
+        int querieIdx = offlineQueries[i].second.second;
+
+        while (idx < n and arr[idx] <= arr[i])
+        {
+            trie.insert(arr[idx]);
+            idx++;
+        }
+
+        if (idx == 0)
+        {
+            ans[querieIdx] = -1;
+        }
+        else
+        {
+            ans[querieIdx] = trie.getMax(xi);
+        }
+    }
+
+    return ans;
 }
--- Trie/421. Maximum XOR of Two Numbers in an Array.cpp
+++ Trie/421. Maximum XOR of Two Numbers in an Array.cpp
@@ -2,7 +2,118 @@
 using namespace std;
 typedef long long int ll;
 
-int findMaximumXOR(vector<int> &nums)
+struct Node
 {
+    Node *links[2];
+
+    bool containsKey(int bit)
+    {
+        return (links[bit] != NULL);
+    }
+
+    Node *get(int bit)
+    {
+        return links[bit];
+    }
+
+    void put(int bit, Node *node)
+    {
+        links[bit] = node;
+    }
+};
+
+class Trie
+{
+private:
+    Node *root;
+
+public:
+    Trie()
+    {
+        root = new Node();
+    }
+
+public:
+    void insert(int num)
+    {
+        Node *node = root;
+
+        for (int i = 31; i >= 0; i--)
+        {
+            int bit = ((num >> i) & 1); // checks if ith bit is set or not
+
+            if (!node->containsKey(bit))
+            {
+                node->put(bit, node);
+            }
+
+            node = node->get(bit);
+        }
+    }
+
+public:
+    int getMax(int num)
+    {
+        Node *node = root;
+
+        int maxNum = 0;
+
+        for (int i = 31; i >= 0; i--)
+        {
+            int bit = ((num >> i) & 1); // checks if ith bit is set or not
+
+            if (node->containsKey(1 - bit)) // if it contains opp. bit
+            {
+                maxNum = maxNum | (1 << i); // get the max
+
+                node = node->get(1 - bit);
+            }
+
+            else
+            {
+                node = node->get(bit); // just go to next node
+            }
+        }
+
+        return maxNum;
+    }
+};
+
+int maxXOR(int n, int m, vector<int> &arr1, vector<int> &arr2)
+{
+    Trie trie;
+
+    for (auto &it : arr1)
+    {
+        trie.insert(it);
+    }
+
+    int maxi = 0;
+
+    for (auto &it : arr2)
+    {
+        maxi = max(maxi, trie.getMax(it));
+    }
+
+    return maxi;
 }
 
+int findMaximumXOR(vector<int> &nums)
+{
+    // 1. Insert all the array elements in trie in the form of binary bits
+    Trie trie;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        trie.insert(nums[i]);
+    }
+
+    int maxi = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        maxi = max(maxi, trie.getMax(nums[i]));
+    }
+    
+    return maxi;
+}
