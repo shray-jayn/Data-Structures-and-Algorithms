b93bf4badbd1b26810b13943e13128e439b8ce57
shray-jayn <shrayjayn@gmail.com>
2022-10-04 14:13:15 +0530

    Stacks and Queues

--- Stacks & Queue/227. Basic Calculator II.cpp
+++ Stacks & Queue/227. Basic Calculator II.cpp
@@ -13,13 +13,16 @@ int calculate(string s)
 
     int no = 0;
     char ps = '+';
+
     stack<int> stk;
 
     for (int i = 0; i < s.length(); i++)
     {
 
         if (isdigit(s[i]))
+        {
             no = no * 10 + (s[i] - '0');
+        }
 
         if (isOperator(s[i]) || i == s.length() - 1)
         {
@@ -28,32 +31,43 @@ int calculate(string s)
             {
                 stk.push(no);
             }
+
             else if (ps == '-')
+            {
                 stk.push(-no);
+            }
+
             else if (ps == '*')
             {
                 int top = stk.top();
                 stk.pop();
                 stk.push(top * no);
             }
+
             else if (ps == '/')
             {
                 int top = stk.top();
                 stk.pop();
                 stk.push(top / no);
             }
+
             no = 0;
             ps = s[i];
         }
     }
+
     int res = 0;
+
     while (!stk.empty())
     {
         res += stk.top();
         stk.pop();
     }
+
     return res;
 }
+
+
 int main()
 {
     string s;
